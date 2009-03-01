ae86174ea8847e3af9fc4078975399935ad54bc5
droyer <droyer@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-01 00:27:51 +0000

    Initial steps to include an AJAX search using eID. Basically for testing eID capabilities for the RSS feeds, which will be part of the next release.
    Generally the code works. However, JS for the FE and he database query ned to be adapted. Since I have no clue how to get prototype/scriptaculous to work, his will be a rough thing :-( <b>Help is welcome...</b>
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dr_wiki/trunk@17359 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
 t3lib_extMgm::addPageTSConfig('
 	# default page TSconfig
@@ -20,4 +21,8 @@ t3lib_extMgm::addTypoScript($_EXTKEY,"setup","
 	tt_content.shortcut.20.0.conf.tx_drwiki_pages = < plugin.".t3lib_extMgm::getCN($_EXTKEY)."_pi1
 	tt_content.shortcut.20.0.conf.tx_drwiki_pages.CMD = singleView
 ",43);
+
+// add handler for eID and ajax based search
+$TYPO3_CONF_VARS['FE']['eID_include']['tx_drwiki_search'] = 'EXT:dr_wiki/lib/class.eid_ajax.search.php';
+
 ?>
